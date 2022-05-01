eb6bd98e14fd13ac166a31292ce8d7e41fa37f61
chandankrishnani <52024480+ChandanKrishnani@users.noreply.github.com>
2022-05-01 16:02:09 +0530

    Create Lesson_1_.sol

---
+++ CryptoZombies/Lesson_1_.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+    }
+    fucntion _geneerateRandomDna(string memory _str) 
+
+    // start here
+
+}
